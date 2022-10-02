0704a68242272b536dbe249b69e4b1f668deb084
xyntechx <xyntechx@gmail.com>
2022-10-02 09:10:27 +0800

    💄 Improve styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
         <main className="flex min-h-screen flex-col items-center justify-center py-2">
             <Head>
                 <title>roygbiv</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon/favicon.ico" />
             </Head>
 
             <h1 className="text-5xl mb-5 font-bold">ROYGBIV</h1>
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
                 </a>
             </Link>
             <Link href="/help">
-                <a className="border-2 border-blue-400 rounded-full py-3 px-5 text-center text-blue-600 text-lg hover:bg-blue-400 hover:text-black transition-colors fixed bottom-10 right-10 font-bold animate-bounce">
+                <a className="border-4 border-blue-600 rounded-full py-1 px-3 text-center text-blue-600 text-lg hover:bg-blue-600 hover:text-white transition-colors fixed bottom-10 right-10 font-bold animate-bounce">
                     ?
                 </a>
             </Link>
